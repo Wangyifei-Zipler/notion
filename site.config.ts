25b31b3a96e68b32e566fff342f59e2b5f96f71d
Zipler <93040184+Wangyifei-Zipler@users.noreply.github.com>
2023-01-25 17:23:35 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '王逸飞の诗笺',
+  name: '王逸飞の诗词站',
   domain: 'https://zipler.notion.site/WangYifei-s-Blog-aa6fdb88d7324945837749f4f1b2051d',
   author: '王逸飞',
 
