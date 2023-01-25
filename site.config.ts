759d5a558962a4372e2c6efa2e64f5cd260d79c5
Zipler <93040184+Wangyifei-Zipler@users.noreply.github.com>
2023-01-25 13:06:18 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -47,8 +47,8 @@ export default siteConfig({
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null,
-
+  pageUrlOverrides: /foo': '067dd719a912471ea9a3ac10710e7fdf';
+/bar': '0be6efce9daf42688f65c76b89f8eb27',
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   //navigationStyle: 'default'
